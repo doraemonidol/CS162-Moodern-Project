4e06fd0913117ab10d092de8d2b866afa337e0ab
Ha Nguyen <21125126@student.hcmus.edu.vn>
2022-03-15 01:18:28 +0700

    output student

--- cs162-moodle-project/cs162-moodle-project/student.cpp
+++ cs162-moodle-project/cs162-moodle-project/student.cpp
@@ -28,7 +28,7 @@ void inpStudents(Students* &stu_head){
         getline(cin,stu_cur->account->firstname,'\n');
         cin >> stu_cur->account->gender;
         //input date
-        //cin>>stu_cur->account->doB->day>> stu_cur->account->doB->month >> stu_cur->account->doB->year;
+        cin>>stu_cur->account->doB.day>> stu_cur->account->doB.month >> stu_cur->account->doB.year;
         //scoreboards
         getline(cin,stu_cur->scoreboards->courseName, '\n');
         cin>> stu_cur->scoreboards->courseID;
@@ -52,5 +52,41 @@ void inpStudents(Students* &stu_head){
         cin>> stu_cur->checkincourse->endTime;
         cin>> stu_cur->checkincourse->startDate.day >> stu_cur->checkincourse->startDate.month>> stu_cur->checkincourse->startDate.year;
         cin>> stu_cur->checkincourse->endDate.day >> stu_cur->checkincourse->endDate.month>> stu_cur->checkincourse->endDate.year;
+    }
+}
+void outStudents(Students* &stu_head){
+    Students* stu_cur = stu_head;
+    while (stu_cur){
+        //student id
+        cout << "Student ID: "<< stu_cur->studentID<< endl;
+        //account 
+        cout << "Username: "<< stu_cur->account->uName << endl;
+        cout << "Password: "<< stu_cur->account->pwd << endl;
+        cout << "Role " << stu_cur->account->role << endl;
+        cout << "Social ID: " << stu_cur->account->socialID << endl;
+        cout << "Name: " << stu_cur->account->lastname <<" "<< stu_cur->account->firstname<< endl ;
+        cout << "Gender: " << stu_cur->account->gender << endl;
+        cout << "DOB: " << stu_cur->account->doB.day << "/" << stu_cur->account->doB.month << "/" << stu_cur->account->doB.year << endl;
+        //scoreboard
+        cout << "Course name: " << stu_cur->scoreboards->courseName<< endl;
+        cout << "Course ID: " << stu_cur->scoreboards->courseID << endl;
+        cout << "Score: " << "Midterm: "<<stu_cur->scoreboards->midtermScore<< " Final: " << stu_cur->scoreboards->finalScore << " Lab: "<< stu_cur->scoreboards->labScore << " Bonus: " << stu_cur->scoreboards->bonusScore << endl;
+        //schedule 
+        cout << "Schedule: ";
+    /*  for (int i=0;i<6;i++){
+            for (int j=0;j<4;j++){
+                cout << stu_cur->schedule[i][j] << setw(5);
+            }
+            cout << endl;
+        }
+    */
+        //check-in course
+        cout << "Course ID: " << stu_cur->checkincourse->courseID<< endl;
+        cout << "Room: " << stu_cur->checkincourse->room<< endl;
+        cout << "Start time: " << stu_cur->checkincourse->startTime << endl;
+        cout << "End time: " << stu_cur->checkincourse->endTime << endl;
+        cout << "Start date: " << stu_cur->checkincourse->startDate.day << "/" << stu_cur->checkincourse->startDate.month <<"/"<< stu_cur->checkincourse->startDate.year << endl;
+        cout << "End date: " << stu_cur->checkincourse->endDate.day << "/" << stu_cur->checkincourse->endDate.month <<"/"<< stu_cur->checkincourse->endDate.year << endl;        
+        stu_cur = stu_cur->next;
     }
 }
