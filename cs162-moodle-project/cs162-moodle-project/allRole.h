07e186aa4d06bcb0d57b20ab704a2b186ac3f73b
Dang Hoang Nhat Hung <doraemonidol@gmail.com>
2022-03-14 23:10:31 +0700

    add the 2 functions committed to allRole.h
    I forgot :v:

--- cs162-moodle-project/cs162-moodle-project/allRole.h
+++ cs162-moodle-project/cs162-moodle-project/allRole.h
@@ -2,3 +2,6 @@
 void viewDate(Date d);
 void viewClass(Classes* cl);
 void viewAccounts(Accounts* acc);
+void viewStudents(Students* studentList, Classes* classes);
+
+Classes* findClass(Students* student, Classes* classes);
