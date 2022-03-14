78985bc36d5279f529322fdd12d49d5704f24de3
casiustruonng <vqtruong21@apcs.fitus.edu.vn>
2022-03-14 19:48:47 +0700

    Change inp,out locations

--- cs162-moodle-project/cs162-moodle-project/initialization.h
+++ cs162-moodle-project/cs162-moodle-project/initialization.h
@@ -1,3 +1,7 @@
 #pragma once
 void inpDate(Date& d);
 void inpAccounts(Accounts*& acc);
+void inpScoreboards(Scoreboards*& Board);
+void outScoreboards(Scoreboards*& Board);
+void inpStaffs(Staffs*& St);
+void outStaffs(Staffs*& St)
