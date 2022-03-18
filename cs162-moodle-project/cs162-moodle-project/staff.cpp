b5f4659e0f92846cc8eb7d78e3ef75322e475b7e
Dang Hoang Nhat Hung <doraemonidol@gmail.com>
2022-03-18 20:55:27 +0700

    add "Delete a Course" function to staff

--- cs162-moodle-project/cs162-moodle-project/staff.cpp
+++ cs162-moodle-project/cs162-moodle-project/staff.cpp
@@ -4,3 +4,31 @@
 #include "initialization.h"
 #include "staff.h"
 #include "student.h"
+
+void deleteCourseByID(Courses* courseList) {
+    string courseID;
+    while (true) {
+        cout << "Enter the course ID (will be deleted): \n>> ";
+        cin >> courseID;
+        Courses* curCourse = courseList;
+        while (curCourse && curCourse->courseID != courseID) {
+            curCourse = curCourse->next;
+        }
+        if (curCourse) {
+            while (curCourse->studentList) {
+                Students* tmp = curCourse->studentList->next;
+                delete curCourse->studentList;
+                curCourse->studentList = tmp;
+            }
+            Courses* tmp = curCourse->next;
+            delete curCourse;
+            curCourse = tmp;
+            cout << "Delete course successfully!\n";
+            cout << "Press any key to Return to Main Screen!";
+            int tmp = getch();
+            return;
+        } else {
+            cout << "Can't find course with ID " << courseID << ". Double check and try again!\n";
+        }
+    }
+}
